2ebb8274c027bd3b645fedaf52e3f302d0d50505
Nate Hanisch <nathan.hanisch86@gmail.com>
2023-01-29 13:07:31 -0500

    added code to handlebars files

--- models/Blog.js
+++ models/Blog.js
@@ -28,6 +28,12 @@ Blog.init(
       },
     },
 
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+
     user_id: {
       type: DataTypes.INTEGER,
       references: {
